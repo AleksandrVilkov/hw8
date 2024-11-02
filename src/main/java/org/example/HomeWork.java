c11fdefe09e14399adf1292f8d86841533828176
Вилков Александр Сергеевич <ASVilkov@sberbank.ru>
2024-11-02 12:25:40 +0300

    Первая задача

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,9 +1,20 @@
 package org.example;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 
 public class HomeWork {
+
+    private final Map<String, BiFunction<ProfitData, String[], ProfitData>> profitAction = Map.of(
+            "BID", HomeWork::bid,
+            "DEL", HomeWork::del,
+            "SALE", HomeWork::sale
+    );
 
     /**
      * <h1>Задание 1.</h1>
@@ -11,7 +22,42 @@ public class HomeWork {
      * <a href="https://acm.timus.ru/problem.aspx?space=1&num=1316">https://acm.timus.ru/problem.aspx?space=1&num=1316</a>
      */
     public Double getProfit(List<String> actionList) {
-        return null;
+        var data = new ProfitData();
+        actionList.forEach(action -> {
+            String[] actionArray = action.split(" ");
+            profitAction.get(actionArray[0]).apply(data, actionArray);
+        });
+
+        return data.getProfit();
+    }
+
+    private static ProfitData bid(ProfitData data, String[] actionArray) {
+        data.getTreap().add(Double.valueOf(actionArray[1]), data.getCount());
+        data.setCount(data.getCount() + 1);
+        return data;
+    }
+
+    private static ProfitData del(ProfitData data, String[] actionArray) {
+        data.getTreap().remove(Double.valueOf(actionArray[1]));
+        return data;
+    }
+
+    private static ProfitData sale(ProfitData data, String[] actionArray) {
+        var node = data.getTreap().split(Double.valueOf(actionArray[1]));
+        var changeResult = Math.min(
+                data.getTreap().inorder(node[1]).size() * 0.01,
+                Integer.parseInt(actionArray[2]) * 0.01
+        );
+        data.setProfit(data.getProfit() + changeResult);
+        return data;
+    }
+    @Getter
+    @Setter
+    public static class ProfitData {
+        Treap<Double> treap = new Treap<>();
+        Double price = 0.0;
+        int count;
+        Double profit = 0.0;
     }
 
     /**
---
+++ src/main/java/org/example/Treap.java
@@ -0,0 +1,147 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@NoArgsConstructor
+public class Treap<T extends Comparable<T>> {
+
+    Node<T> root;
+
+    public void add(T key, Integer index) {
+        root = insert(root, key, index);
+    }
+
+
+    public void remove(T key) {
+        root = deleteNode(root, key);
+    }
+
+    public Node<T>[] split(T key) {
+        return root.split(key);
+    }
+
+
+    public List<Node<T>> inorder(Node<T> cur) {
+        List<Node<T>> res = new ArrayList<>();
+        inorder(cur, res);
+        return res;
+    }
+
+    private Node<T> deleteNode(Node<T> cur, T key) {
+        if (cur == null)
+            return null;
+
+        if (key.compareTo(cur.key) < 0)
+            cur.left = deleteNode(cur.left, key);
+        else if (key.compareTo(cur.key) > 0)
+            cur.right = deleteNode(cur.right, key);
+        else if (cur.left == null) {
+            cur = cur.right;
+        } else if (cur.right == null) {
+            cur = cur.left;
+        } else if (cur.left.priority < cur.right.priority) {
+            cur = leftRotation(cur);
+            cur.left = deleteNode(cur.left, key);
+        } else {
+            cur = rightRotation(cur);
+            cur.right = deleteNode(cur.right, key);
+        }
+        return cur;
+    }
+
+    private Node<T> insert(Node<T> cur, T key, Integer index) {
+        if (cur == null) return new Node<>(key, index);
+
+        if (key.compareTo(cur.key) > 0) {
+            cur.right = insert(cur.right, key, index);
+            if (cur.right.priority < cur.priority) {
+                cur = leftRotation(cur);
+            }
+
+        } else {
+            cur.left = insert(cur.left, key, index);
+            if (cur.left.priority < cur.priority) {
+                cur = rightRotation(cur);
+            }
+
+        }
+        return cur;
+    }
+
+    private Node<T> leftRotation(Node<T> x) {
+        var y = x.right;
+        var T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        return y;
+    }
+
+    private Node<T> rightRotation(Node<T> y) {
+        var x = y.left;
+        var T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        return x;
+    }
+
+    private void inorder(Node<T> cur, List<Node<T>> res) {
+        if (cur == null) return;
+
+        inorder(cur.left, res);
+        res.add(cur);
+        inorder(cur.right, res);
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    public static class Node<T extends Comparable<T>> {
+        static Random RND = new Random();
+        T key;
+        int priority;
+        Node<T> left;
+        Node<T> right;
+
+        public Node(T key, int priority) {
+            this(key, priority, null, null);
+        }
+
+        public Node<T>[] split(T key) {
+            Node<T> tmp = null;
+
+            Node<T>[] res = (Node<T>[]) Array.newInstance(this.getClass(), 2);
+
+            if (this.key.compareTo(key) < 0) {
+                if (this.right == null) {
+                    res[1] = null;
+                } else {
+                    Node<T>[] rightSplit = this.right.split(key);
+                    res[1] = rightSplit[1];
+                    tmp = rightSplit[0];
+                }
+                res[0] = new Node<>(this.key, priority, this.left, tmp);
+            } else {
+                if (left == null) {
+                    res[0] = null;
+                } else {
+                    Node<T>[] leftSplit = this.left.split(key);
+                    res[0] = leftSplit[0];
+                    tmp = leftSplit[1];
+                }
+                res[1] = new Node<>(this.key, priority, tmp, this.right);
+            }
+            return res;
+        }
+    }
+}
